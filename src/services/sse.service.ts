5390bfa4fc5bb88853d884a494be132d8dcc331d
Bahaa Alsharif <bhlshrf@gmail.com>
2023-11-01 11:21:58 +0300

    Update sse.service.ts

--- src/services/sse.service.ts
+++ src/services/sse.service.ts
@@ -10,11 +10,15 @@ const sse = <T>(url: string, onMessage: (data: T) => void): EventSource => {
 
     if (response.statusCodeValue === 200) {
       const ruuterResponse = response.body as RuuterResponse;
-      if (ruuterResponse.data)
+      if (ruuterResponse?.data)
         onMessage(Object.values(ruuterResponse.data)[0] as T);
     }
   };
 
+  eventSource.onopen = () => {
+    console.log("SSE connection Opened");
+  };
+  
   eventSource.onerror = () => {
     console.error('SSE error');
   };
